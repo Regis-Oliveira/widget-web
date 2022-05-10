6e97e7f9a12b67583ac76cb9fd4ec820a2b7efcd
regis-oliveira <regis_crysis@hotmail.com>
2022-05-10 15:36:38 -0300

    chore: change background and text color accordingly the theme system

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -56,7 +56,7 @@ export function WidgetForm() {
   }
 
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">      
+    <div className="bg-white dark:bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">      
       {feedbackSent ? (
         <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
       ) : (
